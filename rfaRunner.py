8edaa6e49313b8f4c43d481b4cd0520d23cd091a
odidenko <odidenko@linkedin.com>
2016-11-02 20:38:36 -0700

    assignment 2

--- rfaRunner.py
+++ rfaRunner.py
@@ -1,6 +1,55 @@
-from rfaUtils import getLog, qaprint, close_log
+'''
+Created on Oct 19, 2016
+@author: sashaalexander
+@author: team 9
+'''
+from rfaUtils import getLog, qaPrint, getLocalEnv, getTestCases
+import sys
+from __builtin__ import str
+# process command line arguments
+def usage():
+    print ('[ERROR]Invalid parameters. Proper usage: rfaRunner.py --testrun=<testRunId>')
 
-log = getLog()
-message = 'It is working, right?'
-qaprint(log, message)
-close_log(log)
+
+if len(sys.argv) < 2:
+    sys.exit(usage())
+
+testName = sys.argv[0].split('/')[-1].replace('.py', '')
+
+try:
+    for i in range(1, len(sys.argv)):
+        arg = sys.argv[i].split('=')
+        if arg[0].lower() == '--testrun':
+            trid = int(arg[1])
+except Exception:
+    sys.exit(usage())
+
+# read properties
+localProperties = getLocalEnv('local.properties')
+
+if localProperties == -1:
+    sys.exit('[ERROR]Could not read properties')
+# get the log file handle
+if 'log_dir' in localProperties.keys():
+    log = getLog(testName, localProperties['log_dir'])
+else:
+    sys.exit("[ERROR]log_dir property is missing")
+
+# exit if log creation failed
+if log == -1:
+    sys.exit("[ERROR]Could not create log file")
+
+# read test cases
+test_cases = getTestCases(trid)
+
+if test_cases == -1:
+    qaPrint(log,'[ERROR]Could not read test cases')
+    sys.exit()
+else:
+    qaPrint(log, 'Got test cases. Testrun id is ' + str(trid))
+    qaPrint(log,str(test_cases))
+
+
+# close the log file if it open
+if not log.closed:
+    log.close()
--- rfaUtils.py
+++ rfaUtils.py
@@ -1,36 +1,82 @@
-import datetime
+'''
+Created on Oct 19, 2016
+
+@author: sashaalexander
+@author: team 9
+'''
+from datetime import datetime
 import os
-from datetime import date
+from _collections import defaultdict
 
 
-def make_sure_path_exists(log_path):
+def getLog(testName, logDir):
+    """
+    Creates logDir directory, if it doesn't exist,
+    creates or opens a log file in 'logs' directory.
+    """
+    # assign a current working directory + '/logs' to log_dir variable (platform independent)
+    log_dir = os.path.join(os.getcwd(), logDir)
+    # or --> script directory: log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
+    # or --> user directory: log_dir = os.path.join(os.path.expanduser("~"), "logs")
+
     try:
-        os.makedirs(log_path)
-    except OSError as e:    
-        print e 
-             
-def get_timestamp():
-    return datetime.datetime.now().strftime('%Y_%m_%d_%H:%M_%S')
-
-def getLog(log_dir='logs'):
-    log_path = os.path.join(os.getcwd(), log_dir)
-    log_name = 'testrun_' + get_timestamp() + '.log'
-    make_sure_path_exists(log_path)
-    try:
-        log_file = open(os.path.join(log_path, log_name), 'a')
-        return log_file
-    except IOError as e:
-        print e
+        # if logs directory(!) doesn't exist, create it
+        if not os.path.isdir(log_dir):
+            os.makedirs(log_dir)
+        # open log file with prefix and timestamp (platform independent) in Append mode
+        log = open(os.path.join(log_dir, testName + "_" + getCurTime("%Y%m%d_%H-%M-%S") + ".log"), "a")
+        return log
+    except (OSError, IOError):
+        # return -1 in case of exception
         return -1
 
 
-def qaprint(log_file_handler, log_entry):
-    if  log_file_handler != -1:
-        new_line = log_entry + ' ' + get_timestamp() + '\n'
-        log_file_handler.write(new_line)
-        print new_line
-    else:
-        print("Could not write to log")
-        
-def close_log(log_file):
-    log_file.close()
+def qaPrint(log, message):
+    """
+    Prints 'timestamp + message' to console and writes it to the log file
+    """
+    # current date and time as string + message. example: [Oct 25 01:52:33.000001] TC1 - Passed
+    log_message = getCurTime("[%b %d %H:%M:%S.%f]") + " " + message
+    # prints log_message
+    print log_message
+    # writes message to a log file
+    log.write(log_message + "\n")
+
+
+def getCurTime(date_time_format):
+    """
+    Returns current date_time as a string formatted according to date_time_format
+    """
+    date_time = datetime.now().strftime(date_time_format)
+    return date_time
+
+def getLocalEnv(propertiesFileName):
+    try: 
+        properties = open(propertiesFileName, "r")
+        props = {}
+        for line in properties:
+            line = line.rstrip()
+            if "=" not in line: continue
+            if line.startswith("#"): continue 
+            k, v = line.split("=", 1)
+            props[k] = v
+        return props 
+    except Exception as e:
+        print e
+        return -1
+    
+   
+def getTestCases(testRunId):
+    TEST_CASE_KEYS = ('tcid', 'rest_URL', 'HTTP_method', 'HTTP_RC_desired', 'param_list')
+    testCases = defaultdict(dict)
+    try:
+        testCasesFile = open(str(testRunId) + '.txt')
+        for line in testCasesFile:
+            tc = line.rstrip().split("|")
+            for i in range(1, len(tc)-1):
+                testCases[tc[0]][TEST_CASE_KEYS[i]] = tc[i]
+            testCases[tc[0]][TEST_CASE_KEYS[-1]]= tc[-1].split(',')
+        return testCases
+    except Exception as ex:
+        print ex
+        return -1
